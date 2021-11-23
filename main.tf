1994e90e178799af99ca256b19e8899f2d7f87fa
Aleksey Mayer <aleks.g.mayer@gmail.com>
2021-11-23 18:11:35 +0300

    Implement module
    Add labels to vpc_network resource.
    Also we decided that labels should be last argument
     in resource definition.

--- main.tf
+++ main.tf
@@ -1,11 +1,12 @@
 resource "yandex_vpc_network" "default" {
-  name = var.name
+  name   = var.name
+  labels = var.labels
 }
 
 resource "yandex_vpc_subnet" "default-subnets" {
   for_each       = var.subnets
   network_id     = yandex_vpc_network.default.id
-  labels         = var.labels
   v4_cidr_blocks = each.value
   zone           = each.key
+  labels         = var.labels
 }
