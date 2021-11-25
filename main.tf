d17addc16d70ad4352419511f0b396eab7605587
Aleksey Mayer <aleks.g.mayer@gmail.com>
2021-11-25 16:25:41 +0300

    Implement module VPC
    Fix creating subnets block.
    Also reformat code in agreement of style guide.

--- main.tf
+++ main.tf
@@ -1,12 +1,16 @@
 resource "yandex_vpc_network" "default" {
-  name   = var.name
+  name = var.name
+
   labels = var.labels
 }
 
-resource "yandex_vpc_subnet" "default-subnets" {
-  for_each       = var.subnets
+resource "yandex_vpc_subnet" "default_subnets" {
+  for_each = var.subnets
+
   network_id     = yandex_vpc_network.default.id
-  v4_cidr_blocks = each.value
-  zone           = each.key
-  labels         = var.labels
+  name           = each.key
+  zone           = each.value.zone
+  v4_cidr_blocks = [each.value.cidr]
+
+  labels = var.labels
 }
