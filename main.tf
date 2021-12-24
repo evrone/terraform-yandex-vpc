b8e49cff1e3d65f4c22e253c357e843112ad8174
Aleksey Mayer <aleks.g.mayer@gmail.com>
2021-12-24 14:35:30 +0300

    Add subnet map to output
    Add output for subnet and id map.
    Rename name of resources and format code.

--- main.tf
+++ main.tf
@@ -1,11 +1,14 @@
-resource "yandex_vpc_network" "default" {
+resource "yandex_vpc_network" "vpc" {
   name = var.name
 }
 
-resource "yandex_vpc_subnet" "default-subnets" {
-  for_each       = var.subnets
-  network_id     = yandex_vpc_network.default.id
-  labels         = var.labels
-  v4_cidr_blocks = each.value
-  zone           = each.key
+resource "yandex_vpc_subnet" "vpc_subnets" {
+  for_each = var.subnets
+
+  network_id     = yandex_vpc_network.vpc.id
+  name           = each.key
+  v4_cidr_blocks = [each.value.cidr]
+  zone           = each.value.zone
+
+  labels = var.labels
 }
--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,9 @@
-# VPC id
 output "vpc_id" {
   description = "The ID of the VPC"
-  value = yandex_vpc_network.default.id
+  value       = yandex_vpc_network.vpc.id
+}
+
+output "subnets_locations" {
+  description = "Mapping Subnet Name to Subnet ID"
+  value       = zipmap(values(yandex_vpc_subnet.vpc_subnets)[*].name, values(yandex_vpc_subnet.vpc_subnets)[*].id)
 }
